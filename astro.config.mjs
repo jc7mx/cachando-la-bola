54686495a42f9834ea28683646312c4d1a41b867
JC <its.jc7mx@outlook.com>
2026-02-08 23:42:42 -0800

    Agregar Astro plugin rehype-external-links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,22 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap()],
+    // Markdown configuration
+    markdown: {
+        rehypePlugins: [
+            [
+                rehypeExternalLinks,
+                {
+                    target: '_blank',
+                    rel: ['nofollow', 'noopener', 'noreferrer']
+                }
+            ]
+        ]
+    }
 });
