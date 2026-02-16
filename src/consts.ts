3fe3b61ad98458f921d408dd2251261e6d293eda
JC <its.jc7mx@outlook.com>
2026-02-16 00:50:01 -0800

    Corregir el Site Title del sitio

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Cachando la bola';
+export const SITE_TITLE = 'Cachando la Bola';
 export const SITE_DESCRIPTION = 'Opinión, reflexión y análisis deportivo desde la magia de los Juegos Olímpicos, la genialidad del Mundial de fútbol y el espectáculo del Super Bowl.';
